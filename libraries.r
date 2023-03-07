02edeff674f4f7d41785671ee9c7c05e08218ed4
Nils Ratnaweera <rata@zhaw.ch>
2023-03-07 12:35:54 +0000

    updated aux files

--- libraries.r
+++ libraries.r
@@ -4,4 +4,11 @@ library("sf")
 library("leaflet")
 library("shiny")
 library("sfarrow")
+library("mapdeck")
+library("stringr")
+
+
+
+set_token("pk.eyJ1IjoicmF0YWEiLCJhIjoiY2tlOGpjZ3FkMXg4MjMxbjc2azI5ZWd6eiJ9.eL9KSA9nViZAZOWooCOhPg")
+
 # library("leaflet-extras")
--- prepare-data.r
+++ prepare-data.r
@@ -7,16 +7,17 @@ st_layers("data-raw/swissboundaries3d_2023-01_2056_5728.shp/swissBOUNDARIES3D_1_
 sisslerfeld <- read_sf("data-raw/swissboundaries3d_2023-01_2056_5728.shp/swissBOUNDARIES3D_1_4_TLM_HOHEITSGEBIET.shp",
                query = "SELECT NAME as name FROM swissBOUNDARIES3D_1_4_TLM_HOHEITSGEBIET WHERE NAME in ('Sisseln',	'Stein (AG)', 'Münchwilen (AG)', 'Eiken')")
 
+
+st_bbox(sisslerfeld)
+#    xmin    ymin    xmax    ymax 
+# 2637380 1263691 2642887 1267625 
+
+
 st_bbox(sisslerfeld)  |>
  st_as_sfc()  |>
  st_as_text()
 
 st_layers("data-raw/SOLKAT_DACH.gpkg")
-read_sf("data-raw/SOLKAT_DACH.gpkg", query = "(SELECT * FROM SOLKAT_CH_DACH LIMIT 1) solar") |>
-
-
-read_sf("data-raw/SOLKAT_DACH.gpkg", 
-query = "(SELECT ST_PolygonFromText('POLYGON((2637380 1263691, 2642887 1263691, 2642887 1267625, 2637380 1267625, 2637380 1263691))') as geom) solar")
 
 solardach <- read_sf("data-raw/SOLKAT_DACH.gpkg", query = "SELECT st_intersection(solar.geom, sisslerfeld.geom) as geom_intersect, * FROM (SELECT * FROM SOLKAT_CH_DACH) solar, (SELECT ST_PolygonFromText('POLYGON((2637380 1263691, 2642887 1263691, 2642887 1267625, 2637380 1267625, 2637380 1263691))') as geom) sisslerfeld WHERE ST_intersects(solar.geom, sisslerfeld.geom)")
 
@@ -31,6 +32,7 @@ sisslerfeld <- sisslerfeld |>
 
 
 write_sf(solardach, "data-processed/prepared_data.gpkg", "solardach", delete_layer = TRUE)
+write_sf(solardach, "data-processed/solardach.geojson")
 
 sfarrow::st_write_parquet(solardach,"data-processed/solardach.parquet")
 
